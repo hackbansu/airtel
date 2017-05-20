c9fd4ba67da7b3f16c9f99a4c5e280df2a82d245
Pawan Bansal <bansal.pawan96@gmail.com>
2017-05-20 22:56:26 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+/**
+ * Created by hackbansu on 20/5/17.
+ */
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('shopping');
+    // res.send('restaurant');
+    // res.send('chemist');
+})
+
+app.listen(3000, function () {
+    console.log("server successfully started at 3000");
+})
